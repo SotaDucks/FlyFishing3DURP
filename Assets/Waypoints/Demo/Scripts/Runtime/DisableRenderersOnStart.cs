205a26aa25d1a756e96348fe06eec061d472e21f
SotaDucks <ziheng.zhang.polimi@gmail.com>
2024-09-19 18:23:54 +0800

    123
    123

---
+++ Assets/Waypoints/Demo/Scripts/Runtime/DisableRenderersOnStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Waypoints.Utility
+{
+    /// <summary>A component that disables all renderers in the parent and child GameObject(s) on Start().</summary>
+    /// Author: Intuitive Gaming Solutions
+    public class DisableRenderersOnStart : MonoBehaviour
+    {
+        // Unity callback(s).
+        void Start()
+        {
+            // Iterate over each Renderer in the component's gameObject and it's children and disable them.
+            foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+            {
+                renderer.enabled = false;
+            }
+        }
+    }
+}
